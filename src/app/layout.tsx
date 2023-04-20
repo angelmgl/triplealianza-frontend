4854556cc75634810c5ec7a956ef611c8a862b03
Angel Alemany <angelemegeele@gmail.com>
2023-04-20 18:26:52 -0400

    Setup colors and fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 
 export const metadata = {
@@ -5,14 +6,19 @@ export const metadata = {
     description: "Under development",
 };
 
+const font = Nunito_Sans({
+    weight: ["400", "700"],
+    subsets: ["latin"], // señala el alfabeto latino
+});
+
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
-            <body>{children}</body>
+        <html lang="es-py">
+            <body className={font.className}>{children}</body>
         </html>
     );
 }
